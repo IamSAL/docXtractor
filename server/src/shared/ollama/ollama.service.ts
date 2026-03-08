2d7eac8749ac32b4aadbb3354046004841207d3a
Sk Salman Ahmed Sabbir <salman.ahmed@ntb.no>
2026-03-08 23:56:11 +0600

    ollama fix

--- server/src/shared/ollama/ollama.service.ts
+++ server/src/shared/ollama/ollama.service.ts
@@ -20,7 +20,7 @@ export class OllamaService {
   constructor(private configService: ConfigService) {
     const host = this.configService.get<string>(
       'OLLAMA_HOST',
-      'http://host.docked.internal:11434',
+      'http://host.docker.internal:11434',
     );
     this.defaultModel = this.configService.get<string>(
       'OLLAMA_DEFAULT_MODEL',
