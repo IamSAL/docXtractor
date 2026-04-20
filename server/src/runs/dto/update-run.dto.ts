3b68737bcaa9901767c610661f0d3fa364342129
salman.ahmed <salman.ahmed@ntb.no>
2026-04-20 22:51:22 +0600

    fix sort config

--- server/src/runs/dto/update-run.dto.ts
+++ server/src/runs/dto/update-run.dto.ts
@@ -1,14 +1,22 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateRunDto } from './create-run.dto';
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsOptional, ValidateNested } from 'class-validator';
+import { IsOptional, ValidateNested, IsArray, IsString } from 'class-validator';
 import { Type } from 'class-transformer';
 import { SortConfig } from '../entities/run.entity';
 
 class SortConfigDto implements SortConfig {
+  @IsArray()
+  @IsString({ each: true })
   referenceValues: string[];
+
+  @IsString()
   matchColumn: string;
+
+  @IsString()
   fileName: string;
+
+  @IsString()
   referenceColumn: string;
 }
 
